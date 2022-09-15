9ef356b08338f8b54d7b150b53b2b12f15a55626
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-09-15 08:50:12 +0000

    added props on functionalgreeting

--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 
-const FunctionalGreetingWithProps = () => <h1>Hello</h1>
+const FunctionalGreetingWithProps = (props, name, age) => {
+    return <h1>Hello, {props.greeting} {props.name}, I see you're {props.age} </h1>;
+}
 
 export default FunctionalGreetingWithProps;
